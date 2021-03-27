83757abd02a08da40c4daa0cbc233fca53911b5f
Wouter-Thys <55975971+Wouter-Thys@users.noreply.github.com>
2021-03-27 04:56:15 +0100

    yesssirrrr

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.admin')
+@section('content')
+
+    <br>
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                <!-- Widget: user widget style 1 -->
+                <div class="card card-widget widget-user">
+                    <!-- Add the bg color to the header using any of the bg-* classes -->
+                    <div class="widget-user-header">
+
+                    </div>
+
+                    <div class="border-top">
+                        <div class="row ">
+                            <div class="col-sm-4 border-right">
+                                <div class="description-block">
+                                    <h5 class="description-header">1234</h5>
+                                    <span class="description-text">INFO</span>
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-4 border-right">
+                                <div class="description-block">
+                                    <h5 class="description-header">1234</h5>
+                                    <span class="description-text">INFO</span>
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-4">
+                                <div class="description-block">
+                                    <h5 class="description-header">1234</h5>
+                                    <span class="description-text">INFO</span>
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                </div>
+                <!-- /.widget-user -->
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
