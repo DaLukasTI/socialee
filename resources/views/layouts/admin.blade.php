96d5c19d32f572f0153599b5c8a02088e6eb520e
Noah Gillard <noah.gillard@student.kdg.be>
2021-03-27 01:26:16 +0100

    loading /home

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,8 +13,8 @@
 <div class="wrapper">
 
     <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__wobble" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <div class="preloader flex-column justify-content-center align-items-center" style="background-color: #70BEBE!important;">
+        <img class="animation__wobble" src="/img/logo.png" alt="AdminLTELogo" width="80">
     </div>
 
     <!-- Navbar -->
@@ -75,8 +75,8 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
-            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <img src="img/logo.png" alt="AdminLTE Logo" style="opacity: .8; width:60px;">
+            <span class="brand-text font-weight-light pl-3">SOCIALEE</span>
         </a>
 
         <!-- Sidebar -->
