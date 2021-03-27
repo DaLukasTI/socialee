941d23480aec8e27552833c579fea4ee5c7cd682
Wouter-Thys <55975971+Wouter-Thys@users.noreply.github.com>
2021-03-27 14:29:33 +0100

    frontend

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,8 +9,8 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     @livewireStyles
 </head>
-<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-<div class="wrapper">
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed " >
+<div class="wrapper" >
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center" style="background-color: #70BEBE!important;">
@@ -18,34 +18,14 @@
     </div>
 
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-dark">
+    <nav class="main-header navbar navbar-expand">
         <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="/home" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="/post" class="nav-link">New Post</a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="/addpost" class="nav-link">Add Post</a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="/timeline" class="nav-link">Timeline</a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="/settings" class="nav-link">Settings</a>
-            </li>
-        </ul>
 
         <!-- Right navbar links -->
         <ul class="navbar-nav ml-auto">
 
             <li class="nav-item">
-                <a class="float-right btn btn-default btn-flat" href="{{ route('logout') }}" onclick="event.preventDefault();
+                <a class="float-right btn btn-block btn-info" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();" style="background-color: #70BEBE; border-color: white;">
                     {{ __('Logout') }}
                 </a>
@@ -61,19 +41,19 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link text-center" style="background-color: #70BEBE;">
-            <img src="img/logo.png" alt="Logo" style="opacity: .8; width:60px;">
+        <a href="index3.html" class="brand-link text-center border-0" style="background-color: #70BEBE;">
+            <img src="img/logo.png" alt="Logo" style="width:60px;">
         </a>
 
         <!-- Sidebar -->
-        <div class="sidebar">
+        <div class="sidebar" style="background-color:#70BEBE;">
             <!-- Sidebar user panel (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex border-0">
                 <div class="image">
                     <img src="{{asset('img/avatar.png')}}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">{{Auth::user()->name}}</a>
+                    <a href="#" class="d-block" style="color: white;">{{Auth::user()->name}}</a>
 
                 </div>
             </div>
@@ -85,39 +65,39 @@
 
 
             <!-- Sidebar Menu -->
-            <nav class="mt-2">
+            <nav class="mt-2" >
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <!-- Add icons to the links using the .nav-icon class
                          with font-awesome or any other icon font library -->
-                    <li class="nav-item menu-open">
+                    <li class="nav-item menu-open" >
 
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="/home" class="nav-link">
+                                <a href="/home" class="nav-link" style="color: white;">
                                     <i class="far fa-circle nav-icon"></i>
-                                    <p>Dashboard</p>
+                                    <p >Dashboard</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="/post" class="nav-link">
+                                <a href="/post" class="nav-link" style="color: white;">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>New Post</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="/addpost" class="nav-link">
+                                <a href="/addpost" class="nav-link" style="color: white;">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Add Post</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="/timeline" class="nav-link">
+                                <a href="/timeline" class="nav-link" style="color: white;">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Timeline</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="/settings" class="nav-link">
+                                <a href="/settings" class="nav-link" style="color: white;">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Settings</p>
                                 </a>
@@ -132,7 +112,7 @@
     </aside>
 
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+    <div class="content-wrapper"style="background: white">
 
         @yield('content')
     </div>
