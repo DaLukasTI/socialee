7ac9f5b01e01bdd801148a9fe40f9dc60acfc51e
Noah Gillard <noah.gillard@student.kdg.be>
2021-03-27 01:19:27 +0100

    styling login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,17 +3,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Log in (v2)</title>
+    <title>Login | Socialee</title>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
-<body class="hold-transition login-page">
+<body class="hold-transition login-page" style="background-color: #518989;">
 <div class="login-box">
     <!-- /.login-logo -->
-    <div class="card card-outline card-primary">
-        <div class="card-header text-center">
-            <h1 ><b>Socialee</b></h1>
+    <div class="card card-outline">
+        <div class="card-header text-center" style="background-color: #70BEBE;">
+            <div class="image">
+                <img src="{{asset('img/logo.png')}}" alt="Logo">
+            </div>
         </div>
         <div class="card-body">
             <p class="login-box-msg">Sign in to start your session</p>
@@ -23,16 +25,16 @@
                 <div class="input-group mb-3">
                     <input type="email" name="email" class="form-control" placeholder="Email">
                     <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
+                        <div class="input-group-text" style="background-color: #70BEBE;">
+                            <span class="fas fa-envelope" style="color: white;"></span>
                         </div>
                     </div>
                 </div>
                 <div class="input-group mb-3">
                     <input type="password" name="password" class="form-control" placeholder="Password">
                     <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
+                        <div class="input-group-text" style="background-color: #70BEBE;">
+                            <span class="fas fa-lock" style="color: white;"></span>
                         </div>
                     </div>
                 </div>
@@ -47,17 +49,17 @@
                     </div>
                     <!-- /.col -->
                     <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">{{ __('Log In') }} </button>
+                        <button type="submit" class="btn btn-block" style="background-color:#70BEBE; color: white;">{{ __('Log In') }} </button>
                     </div>
                     <!-- /.col -->
                 </div>
             </form>
 
             <p class="mb-1">
-                <a href="forgot-password.html">I forgot my password</a>
+                <a href="forgot-password.html" style="color: #70BEBE;">I forgot my password</a>
             </p>
             <p class="mb-0">
-                <a href="/register" class="text-center">Register a new membership</a>
+                <a href="/register" class="text-center" style="color: #70BEBE;">Register a new membership</a>
             </p>
         </div>
         <!-- /.card-body -->
